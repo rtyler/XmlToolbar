b1d51f4ac802633bddb6509478b11c875130f8eb
R. Tyler Ballance <tyler@monkeypox.org>
2006-12-01 02:02:23 +0000

    o Remove copyright information from all source code marked for release into public domain
    git-svn-id: svn://svn.geekisp.com/bleep/trunk/XmlToolbar@5 a256dd88-e320-0410-9161-d397f098afaa

--- source/MainController.m
+++ source/MainController.m
@@ -3,7 +3,6 @@
 //  XmlToolbar
 //
 //  Created by R. Tyler Ballance on 10/22/06.
-//  Copyright 2006 bleep. LLC. All rights reserved.
 //
 
 #import "MainController.h"
--- source/main.m
+++ source/main.m
@@ -3,7 +3,6 @@
 //  XmlToolbar
 //
 //  Created by R. Tyler Ballance on 10/22/06.
-//  Copyright bleep. LLC 2006. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
